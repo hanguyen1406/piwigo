75e70fc5ef2021eb2f0354ef04c3d015e08c9e1a
FIRSha <hgnguyen47347714@gmail.com>
2025-01-02 15:04:03 +0700

    Generate custom image sizes

--- _data/cache/piwigo_latest_news-vn.cache.php
+++ _data/cache/piwigo_latest_news-vn.cache.php
@@ -1,1 +1,1 @@
-a:5:{s:2:"id";s:5:"33997";s:7:"subject";s:48:"Piwigo 15.1.0 : small fixes and what's new popup";s:9:"posted_on";s:10:"1731336283";s:6:"posted";s:38:"Thứ Hai 11 Tháng Mười Một 2024";s:3:"url";s:47:"https://piwigo.org/forum/viewtopic.php?id=33997";}
+a:5:{s:2:"id";s:5:"34069";s:7:"subject";s:56:"Piwigo 15.3.0 : same as 15.2.0 but compatible with PHP 7";s:9:"posted_on";s:10:"1734690919";s:6:"posted";s:37:"Thứ Sáu 20 Tháng Mười Hai 2024";s:3:"url";s:47:"https://piwigo.org/forum/viewtopic.php?id=34069";}
